d3d11b11325c01b5c35cf0496b8d411df5befcb7
br3tts1r4 <kd_zillza2000_tyz9@hotmail.com>
2026-07-22 00:18:57 +1000

    feat: adjusted screen zoom

--- dotfiles/hypr/mornitors.lua
+++ dotfiles/hypr/mornitors.lua
@@ -4,8 +4,9 @@
 
 -- See https://wiki.hypr.land/Configuring/Basics/Monitors/
 hl.monitor({
-    output   = "",
-    mode     = "preferred",
-    position = "auto",
-    scale    = "auto",
+  output   = "",
+  mode     = "preferred",
+  position = "auto",
+  scale    = "1.1",
 })
+
