d7e9d7adda0346e3f3ee594c33886dbbeae9be65
br3tts1r4 <kd_zillza2000_tyz9@hotmail.com>
2026-08-03 23:21:37 +1000

    feat: added hyprpaper config + hyprland tile border style

--- dotfiles/hypr/autostart.lua
+++ dotfiles/hypr/autostart.lua
@@ -9,6 +9,7 @@
 --
 hl.on("hyprland.start", function()
   hl.exec_cmd("waybar")
+  hl.exec_cmd("hyprpaper")
   --   hl.exec_cmd(terminal)
   --   hl.exec_cmd("nm-applet")
   --   hl.exec_cmd("waybar & hyprpaper & firefox")
